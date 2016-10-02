8a984f6cb766546d875e3406236e60a5ea8037df
geoesshaji <geoesshaji@gmail.com>
2016-10-02 20:51:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req, res){
-   res.send("Article one loaded"); 
+   res.sendFile(path.join(_dirname, 'ui', 'article-one.html')); 
 });
 
 app.get('/article-two',function(req, res){
@@ -20,9 +20,6 @@ app.get('/article-three',function(req, res){
    res.send("Article three loaded"); 
 });
 
-app.get('/image', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
