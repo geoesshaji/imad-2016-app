018219638e17bdbdcd3bf12d5e1ca45054da1abf
geoesshaji <geoesshaji@gmail.com>
2016-10-03 20:12:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,5 @@
-console.log('Loaded!');
-
-//change text
-var element = document.getElementById("maintext");
-element.innerHTML = "New value";
-
-var image = document.getElementById("madi");
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    image.style.marginLeft = marginLeft + "px";
-}
-image.onclick = function(){
-    var interval = setInterval(moveRight, 50);
+//Counter code
+var button = document.getElementById("counter");
+button.onclick = function(){
+    
 };
