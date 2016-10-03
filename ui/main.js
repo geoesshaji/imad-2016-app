f57a43006d0673d7893a6d1a7df3286761e8fc5b
geoesshaji <geoesshaji@gmail.com>
2016-10-03 11:27:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ var element = document.getElementById("maintext");
 element.innerHTML = "New value";
 
 var image = document.getElementById("madi");
+var marginLeft = 0;
+function setInterval(){
+    marginLeft = marginLeft + 10;
+    image.style.marginLeft = marginLeft + "px";
+}
 image.onclick = function(){
+    var interval = setInterval(moveRight, 100);
     image.style.marginLeft = "100px";
 };
