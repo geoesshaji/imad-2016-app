0af665e116ac7d140ea5401eb0ac2f5e050d3213
geoesshaji <geoesshaji@gmail.com>
2016-10-03 11:14:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 //change text
 var element = document.getElementById("maintext");
 element.innerHTML = "New value";
+
+var image = document.getElementById("madi");
+image.onclick = function(){
+    image.style.marginLeft = "100px";
+};
