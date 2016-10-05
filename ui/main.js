ecdd93e8e6712eeeaea24e72708baa84f4fc6965
geoesshaji <geoesshaji@gmail.com>
2016-10-05 15:15:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -56,3 +56,38 @@ submit.onclick = function() {
     //Capture the name and render it as list
    
     };
+    
+//code added by myself
+var commentInput = document.getElementById('commentBox');
+var comment = commentInput.value;
+var submitcomment = document.getElementById('comment_btn');
+submitcomment.onclick = function() {
+    //Make a request to server and send name
+     var request = new XMLHttpRequest();
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          //Take some action
+          if(request.status === 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i = 0; i<names.length ; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul =document.getElementById('commentList');
+                ul.innerHTML = list;
+          }
+      }
+        
+    };
+    //Make the request
+    var nameInput = document.getElementById('name');
+     var name = nameInput.value;
+    request.open('GET', 'http://geoesshaji.imad.hasura-app.io/submit-name?name='+ name, true);
+    request.send(null);
+    
+    
+    //Capture the name and render it as list
+   
+    };
